d107bb296d33898053a0006cb16936c9ba26229a
Reshma <95956293+Reshma-fw14@users.noreply.github.com>
2022-05-07 09:57:45 +0530

    db
    db

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,5 +3,4 @@ const mongoose=require("mongoose");
 const connect=()=>{
     return mongoose.connect("mongodb+srv://reshma:gap_123@cluster0.uwl7a.mongodb.net/products?retryWrites=true&w=majority")
 }
-
 module.exports=connect;
