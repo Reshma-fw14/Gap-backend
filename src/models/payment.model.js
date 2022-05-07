beb7a929b7d7bae3ea6601d8fd3899dc6d63f125
Reshma <95956293+Reshma-fw14@users.noreply.github.com>
2022-05-07 13:16:12 +0530

    payment
    payment

--- src/models/payment.model.js
+++ src/models/payment.model.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const paymentSchema = new mongoose.Schema({
-
     name: {type:String, required: true},
     address : {type:String, required: true},
     email: {type:String, required: true},
